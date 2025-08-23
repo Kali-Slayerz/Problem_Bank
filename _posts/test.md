de20b3f1f68ba055194c213a2b4e548533f52f1e
kali-slayerz <kalislayerz@gmail.com>
2025-08-23 17:01:45 -0400

    First md test

--- _posts/test.md
+++ _posts/test.md
@@ -1,1 +1,3 @@
 ## This is a test md file to see how the deployment goes.
+
+Using another test just to test.
---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1755982745',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
